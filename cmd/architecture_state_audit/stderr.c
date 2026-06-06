89c371503e9ce8f52ba3bc6b0da9e788f7a4e1e9
Koji Ishimoto <koji.ishimoto@gmail.com>
2026-06-06 22:13:06 +0900

    Write audit stderr bytes by length

--- cmd/architecture_state_audit/stderr.c
+++ cmd/architecture_state_audit/stderr.c
@@ -3,5 +3,5 @@
 
 MOONBIT_FFI_EXPORT
 void moonbit_architecture_state_audit_stderr_write(moonbit_bytes_t message) {
-  fputs((const char *)message, stderr);
+  fwrite(message, 1, Moonbit_array_length(message), stderr);
 }
